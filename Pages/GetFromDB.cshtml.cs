638e7c7e4d10c25a3b775531bd14206c542f0a7f
Patryk Osak <pionchaoo@gmail.com>
2021-04-07 19:43:46 +0200

    dodanie wyświetlania 10 ostatnich wierszy posortowanych malejąco według daty

---
+++ Pages/GetFromDB.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FizzBuzzik.Data;
+using FizzBuzzik.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FizzBuzzik
+{
+    public class GetFromDBModel : PageModel
+    {
+        private readonly FizzBuzzContext _context;
+        public GetFromDBModel(FizzBuzzContext context)
+        {
+            _context = context;
+        }
+        public IList<FizzBuzz> List { get; set; }
+        public void OnGet()
+        {
+            //var FizzBuzzQuery = from FizzBuzz in _context.FizzBuzz select FizzBuzz;
+            //var Last10 = FizzBuzzQuery.TakeLast(2);
+            //List = Last10.ToList();
+
+
+            //List = _context.FizzBuzz.ToList();
+            List = _context.FizzBuzz.ToList().TakeLast(10).OrderByDescending(x => x.date).ToList();
+        }
+    }
+}
