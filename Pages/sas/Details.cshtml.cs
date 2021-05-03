db4a46d2f9ea8262833e23232632df4349a14799
Patryk Osak <pionchaoo@gmail.com>
2021-05-03 13:41:31 +0200

    update

---
+++ Pages/sas/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using FizzBuzzik.Data;
+using FizzBuzzik.Models;
+
+namespace FizzBuzzik
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly FizzBuzzik.Data.FizzBuzzContext _context;
+
+        public DetailsModel(FizzBuzzik.Data.FizzBuzzContext context)
+        {
+            _context = context;
+        }
+
+        public FizzBuzz FizzBuzz { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            FizzBuzz = await _context.FizzBuzz.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (FizzBuzz == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
---
+++ Pages/sas/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using FizzBuzzik.Data;
+using FizzBuzzik.Models;
+
+namespace FizzBuzzik
+{
+    public class IndexModel : PageModel
+    {
+        private readonly FizzBuzzik.Data.FizzBuzzContext _context;
+
+        public IndexModel(FizzBuzzik.Data.FizzBuzzContext context)
+        {
+            _context = context;
+        }
+
+        public IList<FizzBuzz> FizzBuzz { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            FizzBuzz = await _context.FizzBuzz.ToListAsync();
+        }
+    }
+}
